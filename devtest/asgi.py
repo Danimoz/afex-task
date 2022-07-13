26d96c2030444e704dc5a8504b70cc349b63a279
danimoz <azubuinedaniel05@gmail.com>
2022-07-13 22:52:48 +0100

    asgi setup

--- devtest/asgi.py
+++ devtest/asgi.py
@@ -8,12 +8,14 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
+import django
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import crm.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'devtest.settings.prod')
+django.setup()
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
