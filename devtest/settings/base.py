732f72af276d85c90a6b834b7aa88648f54f850f
danimoz <azubuinedaniel05@gmail.com>
2022-07-13 21:10:25 +0100

    last

--- devtest/settings/base.py
+++ devtest/settings/base.py
@@ -2,13 +2,14 @@ from pathlib import Path
 from celery.schedules import crontab
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-61vr#!3)f^effz!0n3_yi4pdpxzymmxrxi(!9rxf&p(hn1ujx)'
+
+import django
+django.setup()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
