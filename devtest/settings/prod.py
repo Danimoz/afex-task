0e6c6f8769893c487a986f7709f8452159916e0a
danimoz <azubuinedaniel05@gmail.com>
2022-07-13 19:56:26 +0100

    Changed dev setting for db

--- devtest/settings/prod.py
+++ devtest/settings/prod.py
@@ -14,5 +14,16 @@ SECURE_HSTS_SECONDS = 31536000
 SECURE_HSTS_PRELOAD = True
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'afex',
+        'USER': 'postgres',
+        'PASSWORD': 'azubuined201.',
+        'HOST': 'dbs',
+        'PORT': '',
+    }
+}
+
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
